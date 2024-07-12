9aa6a6f9799a3a77ba87dac535bbed4f1033e4df
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-07-12 21:45:07 +0530

    Commit message 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      apiRoutes: true,
+    },
+  };
+  
+  export default nextConfig;
