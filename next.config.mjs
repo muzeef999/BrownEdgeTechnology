bd92b2113950c8ead90e1d2e0a1dde27c4e00d7f
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-07-21 20:16:05 +0530

    first commit sunday9

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+  },
+  useFileSystemPublicRoutes: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/src/app/api/:path*'
+      }
+    ];
+  }
+};
--- src/app/api/server.js
+++ src/app/api/server.js
@@ -8,46 +8,51 @@ import contactRouter from "./routers/contact.js";
 import authRouter from "./routers/auth.js";
 
 
-const server = express();
 
-// const dev = process.env.NODE_ENV !== "production";
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 const port = process.env.PORT || 3000;
 
 connectDB();
 
-
-
-// app.prepare().then(() => {
-//   const server = express();
-//    server.use(bodyParser.json());
-//   server.use(cors());
-//   server.use(express.json());
-//   server.use(express.urlencoded({ extended: false }));
+app.prepare().then(() => {
+  const server = express();
+   server.use(bodyParser.json());
+  server.use(cors());
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: false }));
   
-
-  server.get("/", (req, res) => {
-     res.send("express is working")
-  });
 
 
   // Remove these lines, as Next.js will handle the routing
-  // server.use("/api", blogRouter);
-  // server.use("/api/auth", authRouter);
-  // server.use("/api/contact", contactRouter);
+  server.get("/api/hello", (req, res) => {
+    res.send("welcome to brown edge technology");
+  });
 
-  // // Use Next.js to handle requests
-  // server.get("*", (req, res, next) => {
-  //   return handle(req, res);
-  // });
+  // Remove these lines, as Next.js will handle the routing
+  server.use("/api", blogRouter);
+  server.use("/api/auth", authRouter);
+  server.use("/api/contact", contactRouter);
+
+  // Use Next.js to handle requests
+  server.get("*", (req, res, next) => {
+    return handle(req, res);
+  });
 
   server.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({ message: err.message });
   });
 
+
+
   server.listen(port, () => {
     console.log(`Server Listening to port ${port}`);
   });
+})
+.catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+});
