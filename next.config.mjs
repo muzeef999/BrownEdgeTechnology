d8420448151142063971bbea220eb57218c2dccf
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-07-21 16:52:12 +0530

    first commit sunday2

--- next.config.mjs
+++ next.config.mjs
@@ -4,4 +4,12 @@ export default {
     API_URL: process.env.API_URL,
   },
   useFileSystemPublicRoutes: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/src/app/api/:path*'
+      }
+    ];
+  }
 };
