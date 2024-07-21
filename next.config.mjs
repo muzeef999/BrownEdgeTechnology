f2ca425956eae97f6f034f582505b81ed6df16e5
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-07-21 19:26:30 +0530

    first commit sunday8

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-export default {
-  publicRuntimeConfig: {
-    API_URL: process.env.API_URL,
-  },
-  useFileSystemPublicRoutes: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/src/app/api/:path*'
-      }
-    ];
-  }
-};
+const nextConfig = {};
+
+export default nextConfig;
